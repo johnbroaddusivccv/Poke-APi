22d5f756a213d5bd8cc6f641b7763ad1de48ecfb
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2019-04-10 21:18:18 -0500

    trying to make progress with this api not so sure I am doing this right

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,39 @@
-$(document).on("click", ".form-control") {
-    alert("button");
+$('button').on("click", function(){
+    function addPokeDiv(response) {
+return `<h3>${response}</h3>`
 }
+
+
+
+
+
+function makeImage() {   
+
+    
+var api = "https://pokeapi.co/api/v2/";
+var input = $(".form-control").val();
+var pokemon = "ditto";
+var query = "pokemon/" + input + "/";
+var queryUrl = api + query;
+console.log(queryUrl);
+console.log(input);
+$.ajax({
+    url: queryUrl,
+    method: "GET"
+    }).then(function(response) {
+        for (let i = 0; i < response.length; i++) {
+            console.log(response[i]);
+            console.log(response);
+            addPokeDiv();
+        }
+        console.log(response.name);
+    })
+}
+
+
+
+makeImage();
+
+})
+
+
